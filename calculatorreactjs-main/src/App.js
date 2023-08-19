c8f10f7779ba27faf982400b4947361f6994b1db
Adilbek <139046716+Adilbekabduraimov1@users.noreply.github.com>
2023-08-19 11:41:56 +0500

    Add files via upload

---
+++ calculatorreactjs-main/src/App.js
@@ -0,0 +1,44 @@
+import Keypad from "./keypad"
+
+import './App.css'
+import { useState } from "react"
+
+function App() {
+
+      // the above state is used to save the calculated value
+      let [input, setInput] = useState("")
+      //it is to operate between two values
+      function handleClick(value) {
+
+            setInput(input + value)
+
+      }
+      //it use eval to evaluate and then it adds to input
+      function calculate(value) {
+
+            let outputVal = (input)
+            setInput(outputVal)
+
+      }
+      // clears value  by setting it to empty string
+      function handleClear() {
+
+            setInput("")
+
+      }
+      // we will send all this functions and state to keypad component and invoke it there
+
+      return (
+            <div className="container">
+                  <h1> React caculator</h1>
+                  <div className="caculator">
+                        <input type="text" value={input} className="output" readOnly />
+                        <Keypad handleClick={handleClick} handleClear={handleClear} calculate={calculate} input={input} setInput={setInput}></Keypad>
+
+                  </div>
+
+            </div>
+      )
+}
+
+export default App
